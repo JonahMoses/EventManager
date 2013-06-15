90bba281a8667e772bdb9c1551dd9415853ac575
Jonah <jonahkmoses@gmail.com>
2013-06-15 15:01:16 -0700

    Personal Legislator Letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -19,6 +19,8 @@ puts "Event Manager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
+template_letter = File.read "Form_letter.html"
+
 contents.each do |row|
 	name = row[:first_name]
 	
@@ -26,5 +28,8 @@ contents.each do |row|
 
 	legislators = legislators_by_zipcode(zipcode).join(", ")
 
-	puts "#{name} #{zipcode} #{legislators}"
+	personal_letter = template_letter.gsub('FIRST_NAME', name)
+	personal_letter.gsub!('LEGISLATORS', legislators)
+
+	puts personal_letter
 end
